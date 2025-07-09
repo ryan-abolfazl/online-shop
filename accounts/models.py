fa3e0bdec591992296e5b3467fa452ae06ed6786
Ryan <ryan.abolfazl@gmail.com>
2025-07-09 09:34:40 +0330

    custom user manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
 class User(AbstractBaseUser):
   email = models.EmailField(max_length=255, unique=True)
@@ -7,6 +8,8 @@ class User(AbstractBaseUser):
   full_name = models.CharField(max_length=255)
   is_active = models.BooleanField(default=True)
   is_admin = models.BooleanField(default=False)
+
+  objects = UserManager()
 
   USERNAME_FIELD = 'phone_number'
   REQUIRED_FIELDS = ['email', 'full_name']
