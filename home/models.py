6b7df444fddb27295e900a79fb618c11ea48a66c
Ryan <ryan.abolfazl@gmail.com>
2025-07-11 19:06:16 +0330

    product-detail

--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -33,3 +33,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail' , args=[self.slug,])
